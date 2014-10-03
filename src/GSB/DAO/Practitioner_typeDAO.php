643084ccb424c82b67a91ee5d08def4ac1eaa508
alexis <alexis@Alex>
2014-10-03 14:33:42 +0200

    version 3 03/10/2014 PM

---
+++ src/GSB/DAO/Practitioner_typeDAO.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace GSB\DAO;
+
+use GSB\Domain\Practitioner_typeDAO;
+
+class Practitioner_typeDAO extends DAO
+{
+    
+    public function findAll() {
+        $sql = "select * from practitioner_type order by practitioner_type_name";
+        $result = $this->getDb()->fetchAll($sql);
+        
+        // Converts query result to an array of domain objects
+        $Practitioner_types = array();
+        foreach ($result as $row) {
+            $type_Id = $row['practitioner_type_id'];
+            $Practitioner_types[$type_Id] = $this->buildDomainObject($row);
+        }
+        return $Practitioner_types;
+    }
+
+    /**
+     * Returns the family matching the given id.
+     *
+     * @param integer $id The family id.
+     *
+     * @return \GSB\Domain\Family|throws an exception if no family is found.
+     */
+    public function find($id) {
+        $sql = "select * from practitioner_type where practitioner_type_id=?";
+        $row = $this->getDb()->fetchAssoc($sql, array($id));
+
+        if ($row)
+            return $this->buildDomainObject($row);
+        else
+            throw new \Exception("No practitioner type found for id " . $id);
+    }
+
+    
+    protected function buildDomainObject($row) {
+        $type = new Practitioner_type();
+        $type->setId($row['practitioner_type_id']);
+        $type->setCode($row['practitioner_type_code']);
+        $type->setName($row['practitioner_type_name']);
+        $type->setPlace($row['practitioner_type_place']);
+        return $type;
+    }
+}
